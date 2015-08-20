792778a83675cf5401f6cddc4898ce6dc2ebd5df
El'endia Starman <lee314b@gmail.com>
2015-08-20 16:09:11 -0400

    Key presses are noted.

---
+++ static/starmanite/headerfooter.js
@@ -0,0 +1,21 @@
+// console.log($("#document"));
+
+// $("#document").keypress(function () {
+	// console.log(event.which);
+// });
+
+// document.onload = function() {
+
+// document.keydown( function () {
+	// console.log(event.which);
+// });
+
+// };
+
+// document.ready = function() {
+	
+	// document.keydown(function(event) {console.log(event.which);});
+	
+// }
+
+$(document).keydown(function(event) {console.log(event.which);});
