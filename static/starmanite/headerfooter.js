a0b0ed2dcaa71066f61e09fa68f79f3e97ec554c
El'endia Starman <lee314b@gmail.com>
2015-08-20 16:58:32 -0400

    Correctly identifies my location in the hierarchy.

--- static/starmanite/headerfooter.js
+++ static/starmanite/headerfooter.js
@@ -1,21 +1,48 @@
-// console.log($("#document"));
-
-// $("#document").keypress(function () {
-	// console.log(event.which);
-// });
-
-// document.onload = function() {
-
-// document.keydown( function () {
-	// console.log(event.which);
-// });
-
-// };
-
-// document.ready = function() {
+$(document).keydown(function(event) {
+	console.log(event.which);
+	var tt = $('.tabtable');
+	var level = 1;
+	var cat;
+	var subcat;
 	
-	// document.keydown(function(event) {console.log(event.which);});
+	if (tt.length > 1) {
+		var tds = tt[1].children[0].children[0].children;
+		for (var i=0; i<tds.length; i++) {
+			var elem = tds[i];
+			var atext = elem.children[0].innerHTML;
+			var alink = elem.children[0].href;
+			console.log(atext);
+			
+			if (atext === atext.toUpperCase()) {
+				level = 2;
+				subcat = [tds,i, atext,alink];
+				console.log(subcat);
+			}
+		}
+	}
+	if (tt.length > 0) {
+		var tds = tt[0].children[0].children[0].children;
+		for (var i=0; i<tds.length; i++) {
+			var elem = tds[i];
+			var atext = elem.children[0].innerHTML;
+			var alink = elem.children[0].href;
+			console.log(atext);
+			
+			if (atext === atext.toUpperCase()) {
+				cat = [tds,i, atext,alink];
+				console.log(cat);
+			}
+		}
+	}
 	
-// }
-
-$(document).keydown(function(event) {console.log(event.which);});
+	if (event.which == 87) { //W, up
+		if (tt.length == 1) {
+			window.location.href = $('#logo > a')[0].href;
+		} else if (tt.length == 2) {
+			window.location.href = '';
+		}
+	} else if (event.which == 83) { //S, down
+	} else if (event.which == 65) { //A, left
+	} else if (event.which == 68) { //D, right
+	}
+});
