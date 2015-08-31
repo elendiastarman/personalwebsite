55840c1b805e25db5071fc45af266515c8a75305
El'endia Starman <lee314b@gmail.com>
2015-08-31 19:27:03 -0400

    Javascript works now and I have third-level tabs on '2D Artwork'.

--- static/starmanite/headerfooter.js
+++ static/starmanite/headerfooter.js
@@ -1,70 +1,47 @@
 $(document).keydown(function(event) {
 	var tabs = $('.tabs');
-	var path = $('#path')[0].children;
-	catLink = path[0].href;
-	if (path.length > 1){ subcatLink = path[1].href; } else {subcatLink="";}
-	var level = 1;
-	var cat;
-	var subcat;
+	var path = $('#path > a');
+	var catLinks = [];
 	
-	if (tabs.length > 0) {
-		var lis = tabs[0].children;
-		for (var i=0; i<lis.length; i++) {
-			var elem = lis[i];
-			var atext = elem.children[0].innerHTML;
-			var alink = elem.children[0].href;
-			
-			if (alink === catLink) {
-				cat = [lis,i, atext,alink];
-				$(elem).addClass("current");
-			}
-		}
+	for (var i=0; i<path.length; i++) {
+		catLinks[i] = path[i].href;
 	}
-	if (tabs.length > 1) {
-		var lis = tabs[1].children;
-		for (var i=0; i<lis.length; i++) {
-			var elem = lis[i];
-			var atext = elem.children[0].innerHTML;
-			var alink = elem.children[0].href;
+	
+	var level = 0;
+	var cats = [];
+	
+	for (var j=0; j<tabs.length; j++) {
+		var lis = tabs[j].children;
+		
+		for (var k=0; k<lis.length; k++) {
+			var elem = lis[k];
 			
-			if (alink === subcatLink) {
-				level = 2;
-				subcat = [lis,i, atext,alink];
+			if (elem.children[0].href === catLinks[j]) {
+				cats[j] = [lis,k];
 				$(elem).addClass("current");
+				level = j;
 			}
 		}
 	}
 	
 	if (event.which == 87) { //W, up
-		if (level == 1) {
+		if (level == 0) {
 			window.location.href = $('#logo > a')[0].href;
-		} else if (level == 2) {
-			window.location.href = cat[3];
+		} else {
+			window.location.href = catLinks[level-1];
 		}
 	} else if (event.which == 83) { //S, down
-		if (level == 1 && tabs.length == 2) {
-			window.location.href = tabs[1].children[0].children[0].href;
+		if (level < tabs.length) {
+			window.location.href = tabs[level+1].children[0].children[0].href;
 		}
 	} else if (event.which == 65) { //A, left
-		if (level == 1) {
-			var j = cat[1]-1;
-			if (j < 0) {j = cat[0].length-1}
-			window.location.href = cat[0][j].children[0].href;
-		} else if (level == 2) {
-			var j = subcat[1]-1;
-			if (j < 0) {j = subcat[0].length-1}
-			window.location.href = subcat[0][j].children[0].href;
-		}
+		var j = cats[level][1]-1;
+		if (j < 0) {j = cats[level][0].length-1}
+		window.location.href = cats[level][0][j].children[0].href;
 	} else if (event.which == 68) { //D, right
-		if (level == 1) {
-			var j = cat[1]+1;
-			if (j > cat[0].length-1) {j = 0}
-			window.location.href = cat[0][j].children[0].href;
-		} else if (level == 2) {
-			var j = subcat[1]+1;
-			if (j > subcat[0].length-1) {j = 0}
-			window.location.href = subcat[0][j].children[0].href;
-		}
+		var j = cats[level][1]+1;
+		if (j > cats[level][0].length-1) {j = 0}
+		window.location.href = cats[level][0][j].children[0].href;
 	}
 });
 
